4be72f86f6a2462955c38f45748b02784aef42c7
Nayeli Macias <69863450+nayeliMacias20@users.noreply.github.com>
2022-10-04 18:31:48 -0500

    04/10/2022 v2

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const showVerb = document.getElementById('showVerb');
 const showImage = document.getElementById('showImage');
 const showAudio = document.getElementById('showAudio');
 
-const fisrt = document.getElementById('first-verb');
+const first = document.getElementById('first-verb');
 const second = document.getElementById('second-verb');
 const third = document.getElementById('third-verb');
 const fourth = document.getElementById('fourth-verb');
@@ -23,31 +23,31 @@ console.log(numberOfVerbs);
 
 //Array de números al azar
 let answerRoullete = [0,1,1,1];
-let positionsOfVerbs = [];
+let everyNumberOfVerbs = [];
 //Variable que maneja la respuesta correcta
-let rightAnswerCounter;
+let rightAnswer;
 //Variable que maneja la respuesta correcta
-let allRightAnswers;
+let rightAnswerCounter=0;
 
 //Funcion para que el usuario escuche los audios. AddEvent"Escuchador"
 //responde con el verbo
 next.addEventListener('click', function(){
     ponerVerbo();
-    //next.style.display = 'none';  
+    next.style.display = 'none';  
     //alert('holis') 
 });
 //Funcion para generar los verbos random
 makeRandomList();
 
 //guarda la ultima posición
-let lastPosition = positionsOfVerbs = length - 1;
+let lastPosition = everyNumberOfVerbs = length - 1;
 //Funcion para guardar la ultima posición
 function makeRandomList(){
     for(var i=0;i<numberOfVerbs;i++){
-        positionsOfVerbs.push(i);
+        everyNumberOfVerbs.push(i);
     }
     //Sort es para ordenar para enviarle un núm aleatorios.
-    positionsOfVerbs = positionsOfVerbs.sort(()=>Math.random()-0.5);
+    everyNumberOfVerbs = everyNumberOfVerbs.sort(()=>Math.random()-0.5);
 }
 //Funcion del efecto de cambio de botón 
 function buttonEffect(itsRight, button){
@@ -66,11 +66,41 @@ function buttonEffect(itsRight, button){
             button.classList.remove('wrongAnswer');
         },1000);
     }
-
+//Funcion de poner el verbo
     setTimeout(function(){
         ponerVerbo();
     },1000);
 }
+
+first.addEventListener('click', function(){
+    buttonEffect(isItRight_(first.innerHTML),this);
+});
+second.addEventListener('click', function(){
+    buttonEffect(isItRight_(second.innerHTML),this);
+});
+third.addEventListener('click', function(){
+    buttonEffect(isItRight_(third.innerHTML),this);
+});
+fourth.addEventListener('click', function(){
+    buttonEffect(isItRight_(fourth.innerHTML),this);
+});
+
+function shuffleAnswer(array){
+    let numberOfAnswerButtons = array.length;
+    let randomIndex;
+
+    while(numberOfAnswerButtons !=0){
+        randomIndex = Math.floor(Math.random()*numberOfAnswerButtons);
+        //Intercambiar valores 
+        [array[numberOfAnswerButtons],array[randomIndex]] = [array[randomIndex],array[numberOfAnswerButtons]];
+    }
+    test.innerHTML += array[0]+''+array[1]+''+array[2]+''+array[1]
+}
+
+
+
+
+
 //Funcion de poner el verbo
 //function ponerVerbo(){
 //showVerb.innerHTML = "Deposita 200 bitcoins para jugar"
